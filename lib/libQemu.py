63ec358b0e2a5b0b38129ec4ae2034d062342119
Bastien S <saidibastien@gmail.com>
2021-10-01 01:17:47 +0100

    New Kernel with new patches (EvDev + Custom Boot screen etc..)

--- lib/libQemu.py
+++ lib/libQemu.py
@@ -13,7 +13,7 @@ from time import sleep as delay
 class Qemu():
     def run(self,name):
         name = path.relpath(name, os.getcwd())
-        cmd = f'& "C:\Program Files\qemu\qemu-system-aarch64" -drive file={name},format=raw,if=sd,id=hd-root  -kernel lib/kernel/kernel8.img -M raspi3b -m 1024 -append "rw console=tty0 loglevel=8 root=/dev/mmcblk0p2 fsck.repair=yes net.ifnames=0 rootwait memtest=1 dwc_otg.lpm_enable=0 earlyprintk" -netdev user,id=net0,hostfwd=tcp::8080-:80 -no-reboot -usb -device usb-mouse -device usb-kbd -device usb-net,netdev=net0 -dtb lib/kernel/bcm2710-rpi-3-b.dtb '
+        cmd = f'& "C:\Program Files\qemu\qemu-system-aarch64" -drive file={name},format=raw,if=sd,id=hd-root  -kernel lib/kernel/Kernel_Custom -M raspi3b -m 1024 -append "rw console=tty0 loglevel=8 root=/dev/mmcblk0p2 fsck.repair=yes net.ifnames=0 rootwait memtest=1 dwc_otg.lpm_enable=0 earlyprintk" -netdev user,id=net0,hostfwd=tcp::8080-:80 -no-reboot -usb -device usb-mouse -device usb-kbd -device usb-net,netdev=net0 -dtb lib/kernel/bcm2710-rpi-3-b.dtb '
         #print(cmd)
         p = subprocess.Popen(["powershell.exe", cmd], stdout=sys.stdout)
         p.communicate()
